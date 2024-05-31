135ead32c6a1ff22a91c77105b3162779765b8d3
Rajkishore <mrajkishor331@gmail.com>
2024-06-01 01:22:02 +0530

    init(fn): user domain setup

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # IMS-Invoice-services
 MVP product codebase
+
+**Build the Project:**
+
+```sh
+mvn clean package
+```
+
+**Run the Application Locally:**
+
+```sh
+mvn spring-boot:run
+```
---
+++ src/test/java/com/bebsaa/invoice/functions/UserFunctionsTests.java
@@ -0,0 +1,13 @@
+package com.bebsaa.invoice.functions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserFunctionsTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
