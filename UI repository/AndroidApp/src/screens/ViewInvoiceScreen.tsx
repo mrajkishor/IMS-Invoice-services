b14bc2139d2613e92dacca0d5c8eda24a0008733
Rajkishore <mrajkishor331@gmail.com>
2024-08-14 04:16:03 +0530

    fix(defect#4): fixed Invoice view page (FE)

--- UI repository/AndroidApp/src/screens/ViewInvoiceScreen.tsx
+++ UI repository/AndroidApp/src/screens/ViewInvoiceScreen.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StyleSheet, ScrollView, GestureResponderEvent, Share, Linking } from 'react-native';
-import { Text, Card, Divider, Button, SegmentedButtons, Appbar } from 'react-native-paper';
+import { Text, Card, Divider, Button, SegmentedButtons, Appbar, ActivityIndicator } from 'react-native-paper';
 import { useRoute, RouteProp, useNavigation } from '@react-navigation/native';
 import { RootStackParamList } from '../navigationTypes';
 import { myColors } from '../config/theme';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { INVOICE_PROD_URL } from '../constants/api';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../store/store';
+import { fetchShopRequest } from '../store/actions/shopActions';
+import { fetchUserRequest } from '../store/actions/userActions';
 
 type TemplateSelectorScreenNavigationProp = StackNavigationProp<RootStackParamList, 'TemplateSelector'>;
 type TemplateSelectorScreenRouteProp = RouteProp<RootStackParamList, 'TemplateSelector'>;
@@ -18,6 +21,17 @@ const ViewInvoiceScreen: React.FC = () => {
     const route = useRoute<ViewInvoiceScreenRouteProp>();
     const { invoice } = route.params;
     const navigation = useNavigation<TemplateSelectorScreenNavigationProp>();
+    const dispatch = useDispatch();
+    const { loading: loadingShops, shops, error: errorStateShops } = useSelector((state: RootState) => state?.shops);
+    const { loading: loadingUsers, user: fetchedUser, error: errorStateUsers } = useSelector((state: RootState) => state?.users);
+
+
+    useEffect(() => {
+        // Fetch the shop and user details
+        dispatch(fetchShopRequest(invoice.shopId));
+        dispatch(fetchUserRequest(invoice.userId));
+    }, [dispatch, invoice.shopId, invoice.userId]);
+
 
     const handleOpenLinkInBrowser = async () => {
         const url = `${INVOICE_PROD_URL}/invoice/${invoice.invoiceId}`;
@@ -55,78 +69,89 @@ const ViewInvoiceScreen: React.FC = () => {
                 <Appbar.Content title="Share Invoice" />
             </Appbar.Header>
             <ScrollView>
-                <Card style={styles.card}>
-                    <Card.Title title="Invoice Details" subtitle="Choose templates to preview..." />
-                    <Card.Content>
-                        <View style={styles.section}>
-                            <Text style={styles.label}>Invoice ID:</Text>
-                            <Text style={styles.value}>{invoice.invoiceId}</Text>
-                        </View>
-                        <Divider />
-                        <View style={styles.section}>
-                            <Text style={styles.label}>Shop ID:</Text>
-                            <Text style={styles.value}>{invoice.shopId}</Text>
-                        </View>
-                        <Divider />
-                        <View style={styles.section}>
-                            <Text style={styles.label}>User ID:</Text>
-                            <Text style={styles.value}>{invoice.userId}</Text>
-                        </View>
-                        <Divider />
-                        <View style={styles.section}>
-                            <Text style={styles.label}>Customer Name:</Text>
-                            <Text style={styles.value}>{invoice.customerName}</Text>
-                        </View>
-                        <Divider />
-                        <View style={styles.section}>
-                            <Text style={styles.label}>Customer Address:</Text>
-                            <Text style={styles.value}>{invoice.customerAddress}</Text>
-                        </View>
-                        <Divider />
-                        <View style={styles.section}>
-                            <Text style={styles.label}>Invoicing Date:</Text>
-                            <Text style={styles.value}>{invoice.invoiceDate}</Text>
-                        </View>
-                        <Divider />
-                        <View style={styles.section}>
-                            <Text style={styles.label}>Due Date:</Text>
-                            <Text style={styles.value}>{invoice.dueDate}</Text>
-                        </View>
-                        <Divider />
-                        <View style={styles.section}>
-                            <Text style={styles.label}>Details:</Text>
-                            <Text style={styles.value}>{invoice.details}</Text>
-                        </View>
-                        <Divider />
-                        <View style={styles.section}>
-                            <Text style={styles.label}>Amount:</Text>
-                            <Text style={styles.value}>Rs {invoice.amount}</Text>
-                        </View>
-                        <Divider />
-                        <View style={styles.footer}>
-                            {/* <Button icon="share" mode="outlined" onPress={() => console.log('Share Invoice')}>
+
+                {loadingShops && loadingUsers ? (
+                    <ActivityIndicator animating={true} style={styles.loader} />
+                ) : (
+                    <Card style={styles.card}>
+                        <Card.Title title="Invoice Details" subtitle="Choose templates to preview..." />
+                        <Card.Content>
+                            <View style={styles.section}>
+                                <Text style={styles.label}>Invoice ID:</Text>
+                                <Text style={styles.value}>{invoice.invoiceId}</Text>
+                            </View>
+                            <Divider />
+
+                            <View style={styles.section}>
+                                <Text style={styles.label}>Owner's Name:</Text>
+                                <Text style={styles.value}>{shops[0]?.shopName}</Text>
+                            </View>
+                            <View style={styles.section}>
+                                <Text style={styles.label}>Owner's Address:</Text>
+                                <Text style={styles.value}>{shops[0]?.address}</Text>
+                            </View>
+                            <Divider />
+                            <View style={styles.section}>
+                                <Text style={styles.label}>Owner's Email:</Text>
+                                <Text style={styles.value}>{fetchedUser.email}</Text>
+                            </View>
+                            <Divider />
+                            <View style={styles.section}>
+                                <Text style={styles.label}>Customer Name:</Text>
+                                <Text style={styles.value}>{invoice.customerName}</Text>
+                            </View>
+                            <Divider />
+                            <View style={styles.section}>
+                                <Text style={styles.label}>Customer Address:</Text>
+                                <Text style={styles.value}>{invoice.customerAddress}</Text>
+                            </View>
+                            <Divider />
+                            <View style={styles.section}>
+                                <Text style={styles.label}>Invoicing Date:</Text>
+                                <Text style={styles.value}>{invoice.invoiceDate}</Text>
+                            </View>
+                            <Divider />
+                            <View style={styles.section}>
+                                <Text style={styles.label}>Due Date:</Text>
+                                <Text style={styles.value}>{invoice.dueDate}</Text>
+                            </View>
+                            <Divider />
+                            <View style={styles.section}>
+                                <Text style={styles.label}>Details:</Text>
+                                <Text style={styles.value}>{invoice.details}</Text>
+                            </View>
+                            <Divider />
+                            <View style={styles.section}>
+                                <Text style={styles.label}>Amount:</Text>
+                                <Text style={styles.value}>Rs {invoice.amount}</Text>
+                            </View>
+                            <Divider />
+                            <View style={styles.footer}>
+                                {/* <Button icon="share" mode="outlined" onPress={() => console.log('Share Invoice')}>
                             Share Invoice
                         </Button> */}
-                            <SegmentedButtons
-                                value={"value"}
-                                onValueChange={() => { }}
-                                buttons={[
-                                    {
-                                        icon: 'eye',
-                                        value: 'preview',
-                                        label: 'Preview',
-                                        onPress: handleOpenLinkInBrowser
-                                    },
-                                    {
-                                        icon: 'share',
-                                        value: 'share',
-                                        label: 'Share',
-                                        onPress: handleShare
-                                    }]}
-                            />
-                        </View>
-                    </Card.Content>
-                </Card>
+                                <SegmentedButtons
+                                    value={"value"}
+                                    onValueChange={() => { }}
+                                    buttons={[
+                                        {
+                                            icon: 'eye',
+                                            value: 'preview',
+                                            label: 'Preview',
+                                            onPress: handleOpenLinkInBrowser
+                                        },
+                                        {
+                                            icon: 'share',
+                                            value: 'share',
+                                            label: 'Share',
+                                            onPress: handleShare
+                                        }]}
+                                />
+                            </View>
+                        </Card.Content>
+                    </Card>
+                )}
+
             </ScrollView>
         </>
 
@@ -154,6 +179,9 @@ const styles = StyleSheet.create({
     footer: {
         marginTop: 16,
         alignItems: 'center',
+    },
+    loader: {
+        marginTop: 20,
     },
 });
 
